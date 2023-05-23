4e17e88314c33c1a299c6a2f41285cc3ccf2f773
Emil Szczepaniak <eszczepaniak@media4u.pl>
2023-05-23 13:59:08 +0200

    EmployeeManagementSystem

--- src/main/java/com/example/EmployeeManagementSystem/repository/TeamRepository.java
+++ src/main/java/com/example/EmployeeManagementSystem/repository/TeamRepository.java
@@ -4,4 +4,6 @@ import com.example.EmployeeManagementSystem.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //public interface TeamRepository extends JpaRepository<Team,Long> {
+
 //}
+
